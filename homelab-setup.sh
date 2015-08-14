f05fbd4b349d895bc0771e083702a1f3d4fb89cf
Michael West <michael@mwe.st>
2015-08-14 00:38:56 -0500

    Add automatic, barebones file replacement for config files

---
+++ homelab-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# sudo bash <(curl -s https://github.com/T3hUb3rK1tten/homelab/raw/master/homelab-setup.sh)
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+rm -Rf /home/michael/.ssh
+mkdir /home/michael/.ssh
+curl -L -o /home/michael/.ssh/authorized_keys https://github.com/T3hUb3rK1tten/homelab/raw/master/authorized_keys
+chown -R michael:michael /home/michael/.ssh
+chmod -R 700 /home/michael/.ssh
+chmod 600 /home/michael/.ssh/authorized_keys
+
+curl -L -o /etc/sudoers.d/michael-sudo https://github.com/T3hUb3rK1tten/homelab/raw/master/michael-sudo
+chmod 440 /etc/sudoers.d/michael-sudo
+
+curl -L -o /etc/ssh/sshd_config https://github.com/T3hUb3rK1tten/homelab/raw/master/sshd_config
+chmod 644 /etc/ssh/sshd_config
+service sshd reload
